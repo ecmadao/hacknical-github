f40e7ed2d7da2ea38237c7e9817360feb96432ac
ecmadao <wlec@outlook.com>
2017-08-23 15:17:22 +0800

    WHAT: Change log info
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -4,20 +4,15 @@ import config from 'config';
 const appName = config.get('appName');
 
 const logger = log4js.getLogger(`[${appName.toUpperCase()}]`);
+log4js.configure({
+  appenders: [
+    { type: 'console' }
+  ]
+});
 
 if (!config.logFile) {
-  log4js.configure({
-    appenders: [
-      { type: 'console' }
-    ]
-  });
   logger.setLevel('DEBUG');
 } else {
-  log4js.configure({
-    appenders: [
-      { type: 'file', filename: config.logFile, }
-    ]
-  });
   logger.setLevel('INFO');
 }
 
