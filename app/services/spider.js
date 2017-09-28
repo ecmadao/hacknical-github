6eb4eb1e98b0ae59f8a0d4139d535bca72581375
ecmadao <wlec@outlook.com>
2017-09-29 00:33:10 +0800

    WHAT: A little style change for github calendar
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- app/services/spider.js
+++ app/services/spider.js
@@ -23,12 +23,12 @@ const getCalendar = async (login) => {
 
   const parsed = githubParser(calSvg);
   const currentStreakInfo = parsed.current_streak
-    ? `${format(DATE_FORMAT, new Date(parsed.current_streak_range[0]))} \u2013 ${format(DATE_FORMAT, new Date(parsed.current_streak_range[1]))}`
+    ? `${format(DATE_FORMAT, new Date(parsed.current_streak_range[0]))} ~ ${format(DATE_FORMAT, new Date(parsed.current_streak_range[1]))}`
     : parsed.last_contributed
       ? `Last contributed in ${format(DATE_FORMAT, new Date(parsed.last_contributed))}.`
       : 'Rock - Hard Place';
   const longestStreakInfo = parsed.longest_streak
-    ? `${format(DATE_FORMAT, new Date(parsed.longest_streak_range[0]))} \u2013 ${format(DATE_FORMAT, new Date(parsed.longest_streak_range[1]))}`
+    ? `${format(DATE_FORMAT, new Date(parsed.longest_streak_range[0]))} ~ ${format(DATE_FORMAT, new Date(parsed.longest_streak_range[1]))}`
     : parsed.last_contributed
       ? `Last contributed in ${format(DATE_FORMAT, new Date(parsed.last_contributed))}.`
       : 'Rock - Hard Place';
@@ -37,7 +37,7 @@ const getCalendar = async (login) => {
     <div class="contrib-column contrib-column-first table-column">
       <span class="text-muted">Contributions in the last year</span>\n
       <span class="contrib-number">${parsed.last_year} total</span>\n
-      <span class="text-muted">${getDateBeforeYears(1, DATE_FORMAT)} \u2013 ${format(DATE_FORMAT)}</span>
+      <span class="text-muted">${getDateBeforeYears(1, DATE_FORMAT)} ~ ${format(DATE_FORMAT)}</span>
     </div>
   `);
   cal.append(`
